8df570b9c10aae39909cfc1922865896f275ce03
Justin Vermillion <justin@lockboxsecurity.net>
2020-06-04 14:19:22 -0600

    now performs syntax analysis on TinyDB data

--- analyze.py
+++ analyze.py
@@ -10,72 +10,87 @@ A keyboard interrupt (Ctrl-C) can be used to stop the analysis process and immed
  write any collected information to disk.
 """
 
-# TODO: import os.path
-
+# Standard imports
+import os.path
 import argparse
 import signal
 import sys
 import datetime as dt
+from io import StringIO
 
-# import Google NLP components
+# Google NLP components
 from google.cloud import language
 from google.cloud.language import enums, types
 
-# other imports
+# other third-party imports
 import ujson as json
-# TODO: from tinydb import TinyDB, Query
+from tinydb import TinyDB, Query
+from markdown import Markdown
+from progress.bar import Bar
 
 # Globals
 stdoutOnly = False
-results = {}
-title = ""
-file = False
 
 
-def today():
-    return dt.date.today().isoformat()
+def unmark_element(element, stream=None):
+    if stream is None:
+        stream = StringIO()
+    if element.text:
+        stream.write(element.text)
+    for sub in element:
+        unmark_element(sub, stream)
+    if element.tail:
+        stream.write(element.tail)
+    return stream.getvalue()
 
 
-def writeJson():
+# patching Markdown
+Markdown.output_formats["plain"] = unmark_element
+__md = Markdown(output_format="plain")
+__md.stripTopLevelTags = False
+
+
+def unmark(text):
+    return __md.convert(text)
+
+
+def now():
+    return dt.datetime.now().isoformat()
+
+
+def writeJson(output):
     if stdoutOnly:
         print("\n! No JSON file will be written.\n")
-        print(json.dumps(results, indent=4), "\n")
+        print(json.dumps(output, indent=4), "\n")
     else:
         filename = "./data/{0}_{1}_{2}.json"
-        filename = filename.format(
-            title if title else "textblob", "syntactical_analysis", today()
-        )
+        filename = filename.format("textblob", "syntactical_analysis", now())
 
         print("\n* Writing to file", filename, end=" ... ", flush=True)
         with open(filename, "w") as fp:
-            fp.write(json.dumps(results, indent=4))
+            fp.write(json.dumps(output, indent=4))
 
         print("Write complete.\n")
 
 
 def sigintHandler(signal, frame):
     print("\n\n! SIGINT RECEIVED -- bailing")
-    if not file:
-        writeJson()
     sys.exit(0)
 
 
-# TODO
-def analyze_file(path):
-    pass
-
-
-def analyze_blob(text):
+def gnlp_analyze(document, isFile=True) -> dict:
     client = language.LanguageServiceClient()
-    document = types.Document(content=text, type=enums.Document.Type.PLAIN_TEXT)
 
-    response = client.analyze_syntax(document=document)
+    if not isFile:
+        text = document.lower()
+    else:
+        text = unmark(document["text"]).replace("\n", " ").lower()
 
-    global results
-    results["sentences"] = len(response.sentences)
-    results["token_count"] = len(response.tokens)
-    results["tokens"] = {}
+    request = types.Document(content=text, type=enums.Document.Type.PLAIN_TEXT)
+    response = client.analyze_syntax(document=request)
+
     tokens = {}
+    sorted_tok = {}
 
     for token in response.tokens:
         pos = enums.PartOfSpeech.Tag(token.part_of_speech.tag).name
@@ -89,9 +104,57 @@ def analyze_blob(text):
     sorted_keys = sorted(tokens, key=lambda x: (tokens[x]["count"]), reverse=True)
 
     for k in sorted_keys:
-        results["tokens"][k] = tokens[k]
+        sorted_tok[k] = tokens[k]
 
-    writeJson()
+    data = {}
+
+    if not isFile:
+        data["original"] = document
+
+    data["syntax"] = {
+        "sentences": len(response.sentences),
+        "token_count": len(response.tokens),
+        "tokens": sorted_tok,
+    }
+
+    return data
+
+
+def analyze_file(fpath):
+    if not os.path.isfile(fpath):
+        print("\n! File {0} not found.".format(fpath))
+        exit()
+
+    db = TinyDB(fpath, indent=4)
+    if not db.contains(Query().subreddit):
+        print("\n! Incorrect database type. Exiting.")
+        exit()
+
+    posts_table = db.table("posts")
+    comments_table = db.table("comments")
+
+    with Bar("Analyzing corpus syntax", max=len(posts_table) + len(comments_table)) as bar:
+        for document in posts_table.all():
+            if document["text"] != "":
+                result = gnlp_analyze(document)
+                posts_table.update(
+                    result, doc_ids=[document.doc_id],
+                )
+
+            bar.next()
+
+        for document in comments_table.all():
+            if document["text"] != "":
+                result = gnlp_analyze(document)
+                comments_table.update(
+                    result, doc_ids=[document.doc_id],
+                )
+            bar.next()
+
+
+def analyze_blob(text):
+    result = gnlp_analyze(text, isFile=False)
+    writeJson(result)
 
 
 def main():
@@ -109,7 +172,7 @@ def main():
         "--stdout",
         dest="stdoutOnly",
         action="store_true",
-        help="Specify this option to print gathered data to STDOUT instead of a JSON file.",
+        help="Specify this option to print gathered data to STDOUT instead of a JSON file. Only supports text input.",
     )
 
     args = parser.parse_args()
@@ -117,11 +180,9 @@ def main():
     global stdoutOnly
     stdoutOnly = args.stdoutOnly
 
-    global file
     if args.t:
-        analyze_blob(args.t.lower())
+        analyze_blob(args.t)
     elif args.f:
-        file = True
         analyze_file(args.f)
     else:
         raise Exception("Something went terribly wrong. So sorry.")
